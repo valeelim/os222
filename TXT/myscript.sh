3f08125d922dcd925d741252b674083374ffd5cc
valeelim <valeriansalim@gmail.com>
2022-09-15 14:15:42 +0700

    rerun script

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -12,7 +12,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="60914D29C01C81F1"
+REC2="188BA1076F6A1AFD"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
